00e04dc52f20e208040252bb68c73862e970c24e
KTthecoder <ktthecoder@gmail.com>
2022-08-18 02:12:35 +0200

    ScrollToTop

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 import Footer from './components/Footer/Footer';
+import ScrollToTop from './components/ScrollToTop/ScrollToTop';
 import Navbar from './navigation/Navbar';
 import Navigation from './navigation/Navigation';
 
 function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop/>
       <Navbar/>
       <main>
         <Navigation/>
